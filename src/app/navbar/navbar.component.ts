d369d27fb5db196a23a6bcd3f73663dc05911c90
Koordis <AleksGeorg@outlook.de>
2023-01-08 17:43:59 +0100

    Film Pupup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,4 +14,19 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  fetchRowData(rowVal) {
+    let route = this.router.config.find(r => r.path === 'app-customer-details/:id');
+    route.data =  rowVal;
+
+  // START: One way of using routerLink
+   this.router.navigateByUrl(`${'app-customer-details'}/${rowVal.id}`); 
+   // Uncomment this line and check the result
+  // END: One way of using routerLink */
+
+   // ---- START: Another way of using routerLink
+  // this.router.navigate(['app-customer-details/' + rowVal.id]);
+  // ---- END: Another way of using routerLink 
+
+  };
 }
